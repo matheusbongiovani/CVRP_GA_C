b4ab1f77fa3e4d21cbaa70dd5f0947aab0a494d8
matal <matalbongiovani@gmail.com>
2022-03-05 02:11:30 -0300

    filling with files

---
+++ ponto.c
@@ -0,0 +1,58 @@
+#include "ponto.h"
+
+struct ponto{
+    int id;
+    double x;
+    double y;
+    double demand;
+};
+
+Ponto* criaPonto(int id, double x, double y){
+    /* Aloca novo ponto */
+    Ponto* novo = (Ponto*) malloc(sizeof(Ponto));
+    novo->id = id;
+    novo->x = x;
+    novo->y = y;
+
+    return novo;
+}
+
+void insereDemanda(Ponto * ponto, double demand){
+    ponto->demand = demand;
+};
+
+double retornaRotulo (Ponto * p){
+    return p->id;
+}
+
+double retornaDemanda(Ponto * ponto){
+    return ponto->demand;
+}
+
+void recebeCoordenadas (double coord1, double coord2, Ponto * ponto){
+    ponto->x = coord1;
+    ponto->y = coord2;
+}
+
+double distanciaEntrePontos(Ponto* ponto1, Ponto* ponto2){
+    int i = 0;
+    double Totalsoma = 0, soma = 0;
+
+    /* Realiza o cálculo da distância euclidiana entre dois pontos */
+        soma = ponto1->x - ponto2->x;
+        soma = soma * soma;
+        Totalsoma += soma;
+
+        soma = 0;
+        soma = ponto1->y - ponto2->y;
+        soma = soma * soma;
+        Totalsoma += soma;
+
+    Totalsoma = sqrt(Totalsoma);
+
+    return Totalsoma;
+}
+
+void destroiPonto (Ponto * p){
+    free(p);
+}
---
+++ ponto.h
@@ -0,0 +1,19 @@
+#ifndef VNS_PONTO_H
+#define VNS_PONTO_H
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct ponto Ponto;
+
+void recebeCoordenadas (double coord1, double coord2, Ponto * ponto);
+Ponto* criaPonto(int id, double x, double y);
+double retornaRotulo (Ponto * p);
+void insereDemanda(Ponto * ponto, double demand);
+
+double retornaDemanda(Ponto * ponto);
+double distanciaEntrePontos(Ponto* ponto1, Ponto* ponto2);
+
+void destroiPonto (Ponto * p);
+
+#endif 
